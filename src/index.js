cf5f694151822cbd65bfe0cab9be0cb728d355ee
Anson Guo <anson.guo.921104@gmail.com>
2023-02-03 17:16:50 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputForm = document.getElementById("create-task-form")
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    appendToDo(e.target.newTaskDescription.value)
+  })
+
+  function appendToDo (toDoItem) {
+    let p = document.createElement('p')
+    let button = document.createElement('button')
+    p.textContent = toDoItem
+    button.textContent = "Delete"
+    p.appendChild(button)
+    document.querySelector('#tasks').appendChild(p)
+    button.addEventListener('click', deleteTask)
+  }
+
+ function deleteTask (e) {
+  e.target.parentNode.remove()
+ }
+
 });
